fe912b4770f8eea432c715b5f6a83e3a8df33f2c
Christian Hansen <christian.hansen@danabagus.id>
2025-03-26 11:05:34 +0700

    Add buttons to navigate to other views

--- objectdetect/urls.py
+++ objectdetect/urls.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from yolov12_detect.views import upload_model
+from yolov12_detect.views import detect_objects
 
 urlpatterns = [
-    path('', upload_model),
+    path('', detect_objects),
     path('admin/', admin.site.urls),
     path('object-detect/', include('yolov12_detect.urls')),
 ]
