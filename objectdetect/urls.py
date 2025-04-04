85c72f8035df157f2a06ef69a9a4240ca75327f7
Christian Hansen <53420284+christianciu@users.noreply.github.com>
2025-04-04 13:57:16 +0700

    Add Detection History + Debug toolbar (#6)
    * Multiple Weights Process (single image)
    
    * Add Detection History + Debug toolbar

--- objectdetect/urls.py
+++ objectdetect/urls.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
 from yolov12_detect.views import detect_objects
 
 urlpatterns = [
@@ -11,4 +12,10 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    try:
+        from debug_toolbar.toolbar import debug_toolbar_urls
+        urlpatterns += debug_toolbar_urls()
+    except ImportError:
+        pass
+
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- yolov12_detect/urls.py
+++ yolov12_detect/urls.py
@@ -6,12 +6,22 @@ app_name = 'yolov12_detect'
 
 urlpatterns = [
     path('', views.upload_model, name='home'),  # Start with model upload
+
     path('upload-model/', views.upload_model, name='upload_model'),
     path('select-model/', views.upload_model, name='select_model'),
     path('upload-image/', views.upload_image, name='upload_image'),
     path('select-image/', views.upload_image, name='select_image'),
-    path('detect/', views.detect_objects, name='detect_objects'),
-    path('result/<int:detection_id>/', views.detection_result, name='detection_result'),
 
-    path('multi-weight/', views.multi_weight_detection, name='multi_weight_detection'),
+    path('single/', views.detect_objects, name='detect_objects'),
+    path('single/result/<int:detection_id>/', views.detection_result, name='detection_result'),
+
+    path('multi/', views.multi_weight_detection, name='multi_weight_detection'),
+    path('multi/result/<int:detection_id>/', views.multi_weight_result, name='multi_weight_result'),
+    
+    path('history/', views.detection_history, name='detection_history'),
+
+    # path('detect/', views.detect_objects, name='detect_objects'),
+    # path('result/<int:detection_id>/', views.detection_result, name='detection_result'),
+# 
+    # path('multi-weight/', views.multi_weight_detection, name='multi_weight_detection'),
 ]
